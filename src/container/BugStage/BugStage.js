db69be8ff7d6da8591f933be11d25fa8987469be
BrandonSpitler <brandonfspitler@gmail.com>
2019-10-28 00:16:51 -0400

    basic layout

---
+++ src/container/BugStage/BugStage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const bugStage = (props) => {
+
+    return (
+        <React.Fragment>
+        </React.Fragment>
+    )
+}
+
+export default bugStage;
---
+++ src/container/MainContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class MainContainer extends Component {
+
+    render() {
+        return (
+            <React.Fragment>
+
+            </React.Fragment>
+        );
+    }
+}
+
+export default MainContainer;
